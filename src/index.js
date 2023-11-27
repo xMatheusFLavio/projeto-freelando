08598c5fde8d1ff75d92cbc9614ad35e3500e947
Matheus <matheusflavio888@gmail.com>
2023-11-27 13:19:50 -0300

    alguns estilos globais adicionados

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import RoutesApp from './routes';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Montserrat';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <RoutesApp />
   </React.StrictMode>
 );
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function RoutesApp() {
+  return (
+    <div>
+      <h1>Freelando</h1>
+    </div>
+  );
+}
+
