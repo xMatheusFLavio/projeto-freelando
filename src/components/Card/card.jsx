0d845ba653a7aa3e5134f798da98fd84366c52b2
Matheus <matheusflavio888@gmail.com>
2023-11-30 15:12:47 -0300

    freelando finalizado pt-1

---
+++ src/components/Card/card.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const DivEstilizada = styled.div`
+    padding: 32px;
+    background: #EBEAF9;
+    border: 1px solid #5754ED;
+    border-radius: 16px;
+`
+
+export default function Card () {
+    return (
+        "a"
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RoutesApp from './routes';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Montserrat';
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <RoutesApp />
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
